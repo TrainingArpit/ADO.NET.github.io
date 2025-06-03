7b1bb223efaf23824f88eb788870f91387103f11
Arpit <125648930+TrainingArpit@users.noreply.github.com>
2025-06-03 11:58:02 +0530

    Add files via upload
    Here is a small example how we use Dependency Injection

---
+++ Unity&DependencyInjection/DependencyConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace PracADO.NET
+{
+    public class DependencyConfig
+    {
+        public static IUnityContainer RegisterComponents()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IMessageService, EmailService>();
+            return container;
+        }
+
+    }
+}
---
+++ Unity&DependencyInjection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Unity;
+
+namespace PracADO.NET
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var container = DependencyConfig.RegisterComponents();
+            var service = container.Resolve<IMessageService>();
+            service.SendMessage("Arpit Is Here");
+        }
+    }
+}
